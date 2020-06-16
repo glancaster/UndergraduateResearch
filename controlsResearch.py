e713ca6333bb0cdd3fa4013c145bef47714e9ae1
glancaster <66974768+glancaster@users.noreply.github.com>
2020-06-16 11:38:47 -0400

    test message

--- controlsResearch.py
+++ controlsResearch.py
@@ -2,3 +2,13 @@ import control as c
 import matplotlib.pyplot as plt 
 import numpy as np 
 import cv2 as cv
+
+cam = cv.VideoCapture(0)
+cam.set(3,800)
+cam.set(4,800)
+
+while True:
+    success, img = cam.read()
+    cv.imshow('Video',img)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
