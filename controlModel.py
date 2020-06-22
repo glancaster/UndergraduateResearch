3a8ba6164c4ec46dc7a7e2431642c041dc713f35
glancaster <66974768+glancaster@users.noreply.github.com>
2020-06-22 17:23:46 -0400

    update 6/22

--- controlModel.py
+++ controlModel.py
@@ -3,9 +3,14 @@ import numpy as np
 import random
 import matplotlib as plt
 
-speedref = random.randint(5,10)
-lag = c.tf([0.3,0.7],[9,10,2])
-tf1 = c.tf([3,4],[5,6])
-sys = speedref*tf1*lag
-sysf = c.feedback(sys)
-print(sysf)
+vel1 = random.randint(5,10) #m/s 
+vel2 = vel1 + 0.1*9.81
+height = 2 #m
+time = height/velStart
+mass = 3 #kg
+accel = 9.81 
+force = mass * accel
+velEnd = velStart + time*accel
+var =[vel1,vel2,time,force,velEnd]
+print(var)
+
